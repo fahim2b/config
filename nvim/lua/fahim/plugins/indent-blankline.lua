ee84c601868199e359c0db240dac8a05e3797ec2
fahim2b <130845546+fahim2b@users.noreply.github.com>
2025-09-11 02:39:33 +0600

    Add files via upload

--- nvim/lua/fahim/plugins/indent-blankline.lua
+++ nvim/lua/fahim/plugins/indent-blankline.lua
@@ -1,8 +1,42 @@
+
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  main = "ibl",
-  opts = {
-    indent = { char = "┊" },
+  -- indent guides
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    main = "ibl",
+    opts = {
+      indent = { char = "┊" },
+    },
+  },
+
+  -- animated indent scope like LazyVim default
+  {
+    "echasnovski/mini.indentscope",
+    version = false, -- use latest
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+    config = function(_, opts)
+      require("mini.indentscope").setup(opts)
+
+      -- disable for some filetypes (just like LazyVim does)
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "lazy",
+          "mason",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
   },
 }
